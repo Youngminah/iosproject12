19b8284d65c6183646c92e41309f13f20e38cf6b
Youngminah <bluebelsmudi@gmail.com>
2021-02-01 19:45:33 +0900

    첫번재 앱 완성

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by meng on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
+    }
+    
+
+
+}
+
