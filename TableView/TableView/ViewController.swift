bc1e2d044826a8e650c9bea03fda5fa990cf05a9
Youngminah <bluebelsmudi@gmail.com>
2021-02-16 23:21:34 +0900

    테이블뷰 틀만 만듦

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by meng on 2021/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("클릭되었음.")
+    }
+
+
+}
+
