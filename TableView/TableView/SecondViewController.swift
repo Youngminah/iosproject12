307d99e1eca3a619047462c1c312084615c5dd5a
Youngminah <bluebelsmudi@gmail.com>
2021-02-19 16:22:08 +0900

    미완성 (1/5)

---
+++ TableView/TableView/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  TableView
+//
+//  Created by meng on 2021/02/19.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var personName: UITextField!
+    @IBOutlet weak var personPhoneNum: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func radioGenderClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
+}
--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -9,24 +9,64 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var data = [
+        ["0","1","2"],
+        ["3","4","5"]
+    ]
+    
+    let heardername = ["즐겨찾기","친구"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func addClicked(_ sender: UIButton) {
+        guard let receiveViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else{
+            return
+        }
+        self.navigationController?.pushViewController(receiveViewController, animated: true)
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return data[section].count
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40.0
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return heardername[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        cell.nameLabel.text = data[indexPath.section][indexPath.row]
+//        if indexPath.row == 5 {
+//            cell.backgroundColor = .white
+//        }else {
+//            cell.backgroundColor = .systemBackground
+//        }
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("클릭되었음.")
     }
+}
 
-
+class ListCell: UITableViewCell{
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
 }
 
