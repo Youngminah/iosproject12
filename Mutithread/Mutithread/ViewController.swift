aecebeec49d26327b4fac764f898d8c3b664baa0
Youngminah <bluebelsmudi@gmail.com>
2021-02-24 20:49:54 +0900

    프로젝트만 생성.

---
+++ Mutithread/Mutithread/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mutithread
+//
+//  Created by meng on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
