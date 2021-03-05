422820571bad8c186530dc1aa528d30c3de63a3d
Youngminah <bluebelsmudi@gmail.com>
2021-03-05 16:46:01 +0900

    Add : [OpenAPI] SettingUIViewController 추가

---
+++ OpenAPI/OpenAPI/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  OpenAPI
+//
+//  Created by meng on 2021/03/05.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
